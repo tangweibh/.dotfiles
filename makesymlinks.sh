3d3d1197c0bf0ad0f864eb9ffd502aa684fd690a
Wei Tang <tangweibh@gmail.com>
2018-01-14 16:44:54 -0600

    add ts and trash commands to trash.sh add setting of trash.sh in makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -11,6 +11,7 @@ dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
 files="gitconfig vimrc tmux.conf bash_aliases inputrc"    
+bin_dir=~/bin
 ##########
 
 # create dotfiles_old in homedir
@@ -32,3 +33,13 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# set the trash script
+if [ ! -d $bin_dir ]
+then
+  mkdir $bin_dir
+fi
+echo "Creating symlink to trash.sh in $bin_dir."
+ln -s $dir/trash.sh $bin_dir/trash
+ln -s $dir/trash.sh $bin_dir/ts
+echo "Please add $bin_dir to your PATH!"
--- trash.sh
+++ trash.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# return if no argument is provided
-if [ $# -eq 0 ]
+bn=$(basename $0)
+if [ "$bn" != "ts" ] && [ "$bn" != "trash" ]
 then
-  echo "No argument is provided!"
+  echo "Call via ts or trash!"
   exit 1
 fi
 
@@ -14,6 +14,22 @@ then
   mkdir $trash_dir
 fi
 
-# move files to the trash dir and show the usage
-mv "$@" $trash_dir
-du -sh $trash_dir
+# return if call ts and no argument is provided
+if [ $# -eq 0 ] && [ "$bn" == "ts" ]
+then
+  echo "No argument is provided!"
+  exit 1
+fi
+
+# move files to the trash dir 
+if [ $# -gt 0 ]
+then
+  mv "$@" $trash_dir
+fi
+
+# show the disk usage if call trash
+if [ "$bn" == "trash" ]
+then
+  du -sh $trash_dir
+fi
+
